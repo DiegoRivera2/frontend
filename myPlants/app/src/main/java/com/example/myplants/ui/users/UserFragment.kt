72410187e8904a626c205c7d145fbb63b22ad913
Gabriel Rodriguez <00005118@uca.edu.sv>
2023-07-01 23:52:33 -0600

    Se creo las pantallas principales, plant,users y el menu principl

---
+++ myPlants/app/src/main/java/com/example/myplants/ui/users/UserFragment.kt
@@ -0,0 +1,30 @@
+package com.example.myplants.ui.users
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myplants.R
+
+
+class UserFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user, container, false)
+    }
+
+    companion object {
+
+    }
+}
